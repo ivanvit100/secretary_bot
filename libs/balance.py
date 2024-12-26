ad384c8207d0f2fc1fea2d187c52ceb865d74bee
ivanvit100 <ivanvit100@gmail.com>
2024-12-26 23:04:51 +0300

    logs and report started

--- libs/balance.py
+++ libs/balance.py
@@ -1,26 +1,47 @@
 import os
 import json
 import datetime
-import matplotlib.pyplot as plt
+import numpy as np # type: ignore
+import matplotlib.pyplot as plt # type: ignore
+from sklearn.linear_model import LinearRegression # type: ignore
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 balance_file_path = os.path.join(current_dir, '../data/balance.json')
 
-def get_balance():
+plt.switch_backend('Agg')
+
+def update_current_month_balance():
+    data = get_full_balance()
+    current_month = datetime.datetime.now().strftime('%B')
+    previous_month = (datetime.datetime.now() - datetime.timedelta(days=30)).strftime('%B')
+    previous_balance = data['year'].get(previous_month, {}).get('balance', 0)
+    data['year'][current_month] = {
+        'balance': previous_balance,
+        'saldo': 0
+    }
+    with open(balance_file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+    return previous_balance
+
+def get_balance(month=-1):
     with open(balance_file_path, 'r') as file:
         data = json.load(file)
-    month = datetime.datetime.now().strftime('%B')
-    return data.get("year").get(month, {}).get('balance'), data.get("year").get(month, {}).get('saldo')
+    if month == -1:
+        month = datetime.datetime.now().strftime('%B')
+    return round(data['year'].get(month, {}).get('balance', 0), 2), round(data['year'].get(month, {}).get('saldo', 0), 2)
 
 def get_full_balance():
     with open(balance_file_path, 'r') as file:
         data = json.load(file)
-    return data.get("year", {})
+    return data
 
 def plot_balance(data):
-    months = list(data.keys())
-    balances = [data[month].get('balance', 0) for month in months]
-    saldos = [data[month].get('saldo', 0) for month in months]
+    months = list(data['year'].keys())
+    balances = [data['year'][month].get('balance', 0) for month in months]
+    saldos = [data['year'][month].get('saldo', 0) for month in months]
+
+    balances = np.nan_to_num(balances)
+    saldos = np.nan_to_num(saldos)
 
     plt.figure(figsize=(10, 5))
     plt.plot(months, balances, label='Balance', marker='o')
@@ -34,11 +55,51 @@ def plot_balance(data):
     plt.savefig('balance_plot.png')
     plt.close()
 
+def plot_income_expenses(data):
+    income = data.get('income', 0)
+    expenses = data.get('expenses', 0)
+    labels = ['Income', 'Expenses']
+    sizes = [income, expenses]
+    colors = ['#ff9999','#66b3ff']
+    explode = (0.1, 0)
+
+    sizes = np.nan_to_num(sizes)
+
+    plt.figure(figsize=(8, 8))
+    plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+            autopct='%1.1f%%', shadow=True, startangle=140)
+    plt.axis('equal')
+
+    plt.savefig('income_expenses_plot.png')
+    plt.close()
+
 def update_balance(new_balance):
     with open(balance_file_path, 'r') as file:
         data = json.load(file)
     month = datetime.datetime.now().strftime('%B')
     data['year'][month]['balance'] += new_balance
     data['year'][month]['saldo'] += new_balance
-    with open('./data/balance.json', 'w') as file:
+    with open(balance_file_path, 'w') as file:
         json.dump(data, file, indent=4)
+
+def forecast_balance_and_saldo(data):
+    months = list(data['year'].keys())
+    balances = [data['year'][month].get('balance', 0) for month in months]
+    saldos = [data['year'][month].get('saldo', 0) for month in months]
+
+    balances = np.nan_to_num(balances)
+    saldos = np.nan_to_num(saldos)
+
+    X = np.arange(len(months)).reshape(-1, 1)
+    y_balance = np.array(balances)
+    y_saldo = np.array(saldos)
+
+    model_balance = LinearRegression()
+    model_balance.fit(X, y_balance)
+    forecast_balance = model_balance.predict([[len(months)]])[0]
+
+    model_saldo = LinearRegression()
+    model_saldo.fit(X, y_saldo)
+    forecast_saldo = model_saldo.predict([[len(months)]])[0]
+
+    return forecast_balance, forecast_saldo
--- main.py
+++ main.py
@@ -7,12 +7,10 @@ import os
 from dotenv import load_dotenv
 from libs.balance import *
 
-
 # +TODO: /start - приветствие
 # TODO: /help - список команд
-# TODO: /call - написать владельцу бота
-# TODO: /report - отчёт по балансу за прошлый месяц
-# TODO: /report now - отчёт по балансу за текущий месяц
+# +TODO: /call - написать владельцу бота
+# TODO: /report - отчёт по балансу
 # +TODO: /balance - текущий баланс
 # +TODO: /balance <число> - изменить баланс на число
 # TODO: /notification <дата> <время> <сообщение> - уведомление
@@ -25,10 +23,9 @@ from libs.balance import *
 # TODO: /email list - список сообщений на почту
 # TODO: /save - сохранить файл на веб-сервере
 # TODO: /share - поделиться файлом
-# TODO: /log - лог действий
+# +TODO: /log - лог действий
 # TODO: /ssh <команда> - выполнить команду на сервере
 # TODO: /stats - статистика Beget
-
 
 #########################
 #                       #
@@ -64,7 +61,37 @@ def check(id):
         bot.send_message(id, 'У вас нет прав для выполнения этой команды')
         return False
 
+def report():
+    # TODO: fix forecast and second plot, add more info
+    data = get_full_balance()
+    plot_balance(data)
+    plot_income_expenses(data)
+    forecast_balance, forecast_saldo = forecast_balance_and_saldo(data)
+    caption = f'Отчёт по балансу за прошлый месяц\n\nПрогнозированный баланс: {round(forecast_balance, 2)}\nПрогнозированное сальдо: {round(forecast_saldo, 2)}'
+    
+    media = []
+    media.append(telebot.types.InputMediaPhoto(open('balance_plot.png', 'rb'), caption=caption))
+    media.append(telebot.types.InputMediaPhoto(open('income_expenses_plot.png', 'rb'), caption='Доходы и расходы за прошлый месяц'))
+    
+    bot.send_media_group(USER_ID, media)
+    
+    os.remove('balance_plot.png')
+    os.remove('income_expenses_plot.png')
+    
+    current_balance, _ = get_balance()
+    update_balance(-current_balance)
+    update_current_month_balance()
+
+def everyday_job():
+    day = datetime.datetime.now().strftime('%d')
+    if (day == '01'):
+        report()
+        logging.info('Monthly report was sent')
+    # TODO: todo and notifications
+
 def main():
+    everyday_job()
+    schedule.every().day.at("00:00").do(everyday_job)
     bot.send_message(USER_ID, 'Секретарь запущен')
     logging.info('Secretary bot started')
     while True:
@@ -73,14 +100,6 @@ def main():
         except Exception as e:
             print(f"Ошибка: {e}")
             time.sleep(15)
-
-#########################
-#                       #
-#       SCHEDULE        #
-#                       #
-#########################
-
-
 
 #########################
 #                       #
@@ -102,18 +121,14 @@ def balance(message):
         return
     message_parts = message.text.split(' ')
     if len(message_parts) == 1:
-        try:
-            data = get_full_balance()
-            plot_balance(data)
-            current_balance, current_saldo = get_balance()
-            month = datetime.datetime.now().strftime('%B')
-            caption = f'Текущий месяц: {month}\n\nВаш баланс: {current_balance}\nСальдо: {current_saldo}'
-            with open('./balance_plot.png', 'rb') as photo:
-                bot.send_photo(message.from_user.id, photo, caption=caption)
-            os.remove('./balance_plot.png')
-        except Exception as e:
-            bot.send_message(message.from_user.id, f'Произошла ошибка')
-            logging.error(f'Error: {e}')
+        data = get_full_balance()
+        plot_balance(data)
+        current_balance, current_saldo = get_balance()
+        month = datetime.datetime.now().strftime('%B')
+        caption = f'Текущий месяц: {month}\n\nВаш баланс: {current_balance}\nСальдо: {current_saldo}'
+        with open('balance_plot.png', 'rb') as photo:
+            bot.send_photo(message.from_user.id, photo, caption=caption)
+        os.remove('balance_plot.png')
     else:
         try:
             new_balance = float(message_parts[1])
@@ -121,6 +136,29 @@ def balance(message):
             bot.send_message(message.from_user.id, f'Баланс обновлен: {new_balance}')
         except ValueError:
             bot.send_message(message.from_user.id, 'Неверный формат параметра')
+
+@bot.message_handler(commands=['report'])
+def rpt(message):
+    if not check(message.from_user.id):
+        return
+    report()
+
+@bot.message_handler(commands=['call'])
+def call(message):
+    message_parts = message.text.split(' ')
+    if message.from_user.id != int(USER_ID) and len(message_parts) > 1:
+        bot.send_message(int(USER_ID), f'Анонимное сообщение: \n\n{message.text[6:]}')
+        bot.send_message(message.from_user.id, 'Сообщение отправлено')
+
+@bot.message_handler(commands=['log'])
+def log(message):
+    if not check(message.from_user.id):
+        return
+    with open('./secretary.log', 'r') as file:
+        lines = file.readlines()
+        log = ''.join(lines[-25:])
+    bot.send_message(message.from_user.id, log)
+
 
 
 if __name__ == '__main__':
